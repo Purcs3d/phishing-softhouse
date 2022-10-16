0167175cbac6a21b64ea674f96066a61e8854f02
mdzwzw <md.zmzm.1991@gmail.com>
2022-10-16 11:42:25 +0200

    frontend-backend1

--- URL-analyzer/main.py
+++ URL-analyzer/main.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import src.algorithmManager as am
-
+from website import create_app
 
 def main():
+
+    app = create_app()
+    app.run(debug=True)
     #Website
     URLstring = "https://www.sweclockers.com/nyhetsbrev"
 
