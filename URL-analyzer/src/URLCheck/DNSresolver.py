ad9ed3bb19d758159da35066e227694e6c41735d
itggot-lukas-einlerlarsson <lukas.einlerlarsson@itggot.se>
2022-10-21 15:38:24 +0200

    Fixed IP address fetch from website in DNS resolver

--- URL-analyzer/src/URLCheck/DNSresolver.py
+++ URL-analyzer/src/URLCheck/DNSresolver.py
@@ -21,6 +21,10 @@ class DNSresolver():
             return self.URLinfo
         data = requests.get(f"https://geolocation-db.com/json/{ip}&position=true").json()
         self.URLinfo.data = requests.get(f"https://geolocation-db.com/json/{ip}&position=true").json()
+        if "IPv4" in data.keys():
+            self.URLinfo.ip = data["IPv4"]
+        if "IPv6" in data.keys():
+            self.URLinfo.ip = data["IPv6"]
         self.URLinfo.country = data['country_name']
         self.URLinfo.city = data['city']
         self.URLinfo.region = data['state']
