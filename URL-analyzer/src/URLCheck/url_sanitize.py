19fad239cbc62450fa679ae940cdd2a32e3d21e9
Purcs3d <totte.kontakt@gmail.com>
2022-11-10 11:24:34 +0100

    Quick backup

--- URL-analyzer/src/URLCheck/url_sanitize.py
+++ URL-analyzer/src/URLCheck/url_sanitize.py
@@ -4,12 +4,60 @@ in regards to URL
 """
 
 import requests
+from url_parser import get_url
+from urllib import request
 
-def addScheme(url:str) -> str:
+def siteValid(url:str, redir:bool = True) -> bool:
+    """
+    Checks if a site exists and is reachable
+    Requires supplied schema
+    """
+    url = addScheme(url, redir)
+
+    site_code = 0
+    try:
+        site_code = request.urlopen(url).getcode()
+        print(site_code)
+
+        # check if site response is within OK range
+        if 200 <= site_code <= 299:
+            return True
+
+    except Exception as err:
+        code = err.__dict__['code']
+        # Check if server or client error occured (raise error in case of client error)
+        if 500 <= code <= 599:
+            return False
+
+        else:
+            raise err
+
+
+def addScheme(url:str, fetch:bool = True) -> str:
     """ 
     Checks if the address holds a
     """
-    ...
+    schema = get_url(url).protocol
+    if schema == None:
+        # add http as "starting point"
+        prot_url = 'http://' + url
+    
+    # fetch server requested scheme (if reachable)
+    if fetch:
+        try:
+            prot_url = getRedir(prot_url)
+        
+        except:
+            # try reachability using 'https' if 'http' fail
+            try:
+                prot_url = 'https://' + url
+                prot_url = getRedir(prot_url)
+
+            # return error in case http types fail
+            except Exception as err:
+                raise err
+
+    return url
 
 
 def getRedir(addr:str) -> str:
@@ -20,5 +68,6 @@ def getRedir(addr:str) -> str:
     connection = requests.get(addr)
     return connection.url
 
+URL  = 'www.amazon.com'
 
-print(getRedir("googl.com"))
+print(siteValid(URL))
