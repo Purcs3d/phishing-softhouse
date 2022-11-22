a14d5f2e6d8ed6c70a1cfedca5cf904704bb0501
itggot-lukas-einlerlarsson <lukas.einlerlarsson@itggot.se>
2022-11-22 15:29:02 +0100

    renamed db_handler to DBhandler.py

--- URL-analyzer/src/DB/DBhandler.py
+++ URL-analyzer/src/DB/DBhandler.py
@@ -29,31 +29,49 @@ CREATE SCHEMA IF NOT EXISTS URLanalyzer;
 
 CREATE TABLE IF NOT EXISTS URLanalyzer.whitelist (
 	ID 	SERIAL 	PRIMARY KEY,
-	URL CHAR(200)
+	URL TEXT
 );
 
 CREATE TABLE IF NOT EXISTS URLanalyzer.previousSearches (
 	ID 	SERIAL 	PRIMARY KEY,
 	searchDate DATE DEFAULT CURRENT_DATE,
-	URL CHAR(200),
-	report CHAR(200)
+	URL TEXT,
+	report TEXT
 );
 
 
+CREATE TABLE IF NOT EXISTS URLanalyzer.test (
+	ID 	SERIAL 	PRIMARY KEY
+);
+select * from URLanalyzer.test;
+select count(*) from urlanalyzer.test; -- if empy table -> 0
+drop table URLanalyzer.test;
+
 select * from urlanalyzer.previoussearches;
 
-create or replace function checkAge(url char(200), curDate DATE default CURRENT_DATE)
-returns bool
-as $$
-begin
-	-- declare:
-	declare oldDate DATE;
+select * from urlanalyzer.previoussearches;
 
-	Select searchDate
-    select
-    commit;
-end;$$
-language plpgsql
+insert into urlanalyzer.previoussearches(URL, report)
+VALUES ('youtube.com', 'report');
+
+-- for each new insert, remove searches older than 3 days
+CREATE OR REPLACE FUNCTION oldSearchesFunc()
+  RETURNS TRIGGER
+  LANGUAGE PLPGSQL
+  AS
+$$
+BEGIN
+	delete from urlanalyzer.previoussearches
+	where extract(day from CURRENT_DATE)- extract( day from searchDate) > 3;
+	RETURN NULL;
+END;
+$$
+create trigger removeOldSearches
+after insert on urlanalyzer.previoussearches
+EXECUTE PROCEDURE oldSearchesFunc();
+
+drop function oldSearchesFunc;
+drop trigger removeOldSearches;
 
 -- drop table urlanalyzer.whitelist;
 -- truncate table urlanalyzer.whitelist;
@@ -64,30 +82,24 @@ language plpgsql
         """
 
     def init_createWhitelist(self):
-        sql_str = "select count(url) from URLanalyzer.whitelist;"
+        sql_str = "select count(*) from URLanalyzer.whitelist;"
         self.cursor.execute(sql_str)
-        print(self.cursor.fetchone())
-        # if self.cursor.fetchone()[0] == 0:
-            # print("whitelist is empty")
-        sql_str = "select count(url) from URLanalyzer.previousSearches;"
-        self.cursor.execute(sql_str)
-        print(self.cursor.fetchone())
-        # if self.cursor.fetchone()[0] == 0:
-            # print("previousSearches is empty")
-        # with open("whitelist.txt") as file:
-        #     for line in file:
-        #         sql_str = f"INSERT INTO URLanalyzer.whitelist (URL) VALUES ('{line.rstrip()}');"
-        #         self.cursor.execute(sql_str)
-        #     self.conn.commit()
+        if self.cursor.fetchone()[0] != 0:
+            print("Whitelist already exist")
+            return
+        else:
+            with open("whitelist.txt") as file:
+                for line in file:
+                    sql_str = f"INSERT INTO URLanalyzer.whitelist (URL) VALUES ('{line.rstrip()}');"
+                    self.cursor.execute(sql_str)
+                self.conn.commit()
 
     def checkURLinWhitelist(self, url):
+        """
+            Checks if URL exist in whitelist
+            input: URL, output: if it exist(True) or not (False)
+        """
         sql_str = f"select * from URLanalyzer.whitelist where url LIKE '%{url}%';"
-        self.cursor.execute(sql_str)
-        if self.cursor.fetchone() == None:
-            print(f"Url not found in whitelist: {url}")
-
-    def checkURLinpreviousSearches(self, url):
-        sql_str = f"select * from URLanalyzer.previousSearches where url = '{url}';"
         self.cursor.execute(sql_str)
         if self.cursor.fetchone() == None:
             print(f"Url not found in whitelist: {url}")
@@ -95,15 +107,34 @@ language plpgsql
         else:
             return True
 
-
-
-    def insertIntopreviousSearches(self, url, report):
+    def checkURLinpreviousSearches(self, url):
+        """
+            Checks if URL exist in previousSearches table
+            input: URL, output: if it exist(True) or not (False)
+        """
         sql_str = f"select * from URLanalyzer.previousSearches where url = '{url}';"
         self.cursor.execute(sql_str)
         if self.cursor.fetchone() == None:
-            print(f"Url not found in whitelist: {url}")
+            print(f"Url not found in previousSearches: {url}")
+            return False
         else:
-            pass
+            return True
+    def fetchPreviousSearchReport(self, url):
+        """
+            Fetches report from a url found in previousSearches table.
+            ! Used in combination with the function checkURLinpreviousSearches !
+            input: URL, output: report of previously searched URL
+        """
+        sql_str = f"select report from URLanalyzer.previousSearches where url = '{url}';"
+        self.cursor.execute(sql_str)
+        return self.cursor.fetchone()[0]
+
+    def insertIntopreviousSearches(self, url, report):
+        """
+            This function inserts a url into the database
+            There is a trigger in the database that deletes url searches older than 3 days after insert
+            input: URL, and its report
+        """
         sql_str = f"INSERT INTO URLanalyzer.previousSearches (URL, report) VALUES ('{url}', '{report}');"
         self.cursor.execute(sql_str)
         self.conn.commit()
@@ -123,11 +154,12 @@ language plpgsql
 
 def testium():
     DBtester = DBhandler()
-    DBtester.init_createWhitelist()
-    DBtester.checkURLinWhitelist("svt.se")
-    DBtester.checkURLinWhitelist("medium.com")
-    DBtester.checkURLinWhitelist("medium.com/1231231 ta in subdomain+domain+topdomain")
-    DBtester.checkURLinpreviousSearches("svt.se")
-    DBtester.test()
+    # DBtester.init_createWhitelist()
+    # DBtester.checkURLinWhitelist("svt.se")
+    # DBtester.checkURLinWhitelist("medium.com")
+    # DBtester.checkURLinWhitelist("medium.com/1231231 ta in subdomain+domain+topdomain")
+    if DBtester.checkURLinpreviousSearches("svt.se"):
+        print(DBtester.fetchPreviousSearchReport("svt.se"))
+    # DBtester.test()
 
 testium()
