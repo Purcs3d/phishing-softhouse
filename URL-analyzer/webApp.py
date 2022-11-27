9054983b4e4b9f769f07753f0869752f2fee8ce3
mdzwzw <md.zmzm.1991@gmail.com>
2022-11-28 00:37:16 +0100

    Fixing Output Errors Completed

--- URL-analyzer/webApp.py
+++ URL-analyzer/webApp.py
@@ -11,13 +11,16 @@ def index():
 
 @app.route("/check", methods=['GET', 'POST'])
 def CheckURL():
-    URLinput = str(request.form['URL_input'])
-    if validators.domain(URLinput) == True or validators.url(URLinput) == True:
-        algorithmEngine = am.algorithmManager(URLinput) #algorithm object
-        output = "fishy?:" + str(algorithmEngine.run()) #fishy or not fishy boolean
-        output += "<br> evaluation points:" + str(algorithmEngine.points)
-        output += algorithmEngine.createOutputString()
-        flash(output)
-    else:
-        flash("Please enter a valid URL")
+    try:
+        URLinput = str(request.form['URL_input'])
+        if validators.domain(URLinput) == True or validators.url(URLinput) == True:
+            algorithmEngine = am.algorithmManager(URLinput) #algorithm object
+            output = "fishy?:" + str(algorithmEngine.run()) #fishy or not fishy boolean
+            output += "<br> evaluation points:" + str(algorithmEngine.points)
+            output += algorithmEngine.createOutputString()
+            flash(output)
+        else:
+            flash("Please enter a valid URL")
+    except Exception:
+        flash("Something is broken in the backend")
     return render_template("index.html")
