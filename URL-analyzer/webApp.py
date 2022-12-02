b4c8a154a7d209c20fe5e9a97943fa79e1e36cfb
Raxus00 <rasmus.andersen00@gmail.com>
2022-12-02 13:22:58 +0100

    changes

--- URL-analyzer/webApp.py
+++ URL-analyzer/webApp.py
@@ -2,19 +2,19 @@ import src.algorithmManager as am
 from flask import Flask, flash, render_template, request, Blueprint
 import validators
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'HakunaMatataMasualaYoteYatatatuliwa'
+app = Flask(__name__)   
+app.config['SECRET_KEY'] = 'HakunaMatataMasualaYoteYatatatuliwa'    
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+@app.route("/") 
+def index():    
+    return render_template("index.html")    
 
-@app.route("/check", methods=['GET', 'POST'])
-def CheckURL():
+@app.route("/check", methods=['GET', 'POST'])   
+def CheckURL(): 
     try:
-        URLinput = str(request.form['URL_input'])
-        if validators.domain(URLinput) == True or validators.url(URLinput) == True:
-            algorithmEngine = am.algorithmManager(URLinput) #algorithm object
+        URLinput = str(request.form['URL_input'])   
+        if validators.domain(URLinput) == True or validators.url(URLinput) == True: 
+            algorithmEngine = am.algorithmManager(URLinput) 
             fishy = algorithmEngine.run()
             output = algorithmEngine.createOutputString()
             flash(output)
