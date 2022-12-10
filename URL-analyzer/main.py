ba6bf4ec8f014780a12ebbb1b147f4f558e2bd63
Totte Hansen <totte.kontakt@gmail.com>
2022-12-10 17:57:10 +0100

    Fixed debug terminal

--- URL-analyzer/main.py
+++ URL-analyzer/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# import src.algorithmManager as am
+
 import src.algorithmManager as am
 from  webApp import app
 from sys import argv
@@ -8,7 +8,34 @@ from sys import argv
 #! PLEASE STOP USING MULTIPROCESS WHEN NOT NEEDED!!! IT HAS BUGS !#
 
 def main():
-    if argv[1] == "help":
+    # run as Flask webUI
+    if len(argv) < 2 or argv[1] == "server":
+        app.run()
+        # Process(target=app.run()).start()
+
+    # run as terminal UI 
+    elif len(argv) > 0 and argv[1] == "terminal":
+        URL = ""
+
+        while URL != "q":
+            try:
+                URL = input('\nEnter q to Quit:\
+                             \n````````````````\
+                             \nEnter your URL: ')
+
+                algorithmEngine = am.algorithmManager(URL) #algorithm object
+
+                output  = "\nFishy?:" + str(algorithmEngine.run()) #fishy or not fishy boolean
+                output += "\nEvaluation points:" + str(algorithmEngine.points)
+                outputDict = algorithmEngine.createOutputString()
+                
+                for i in outputDict:
+                    print(i, outputDict[i])
+
+            except Exception as e:
+                print(f"Error: {e}")
+
+    else:
         print("Arguments:\n")
         print(f"Run as webUI: server")
         print(f"")
@@ -16,41 +43,6 @@ def main():
         # sys.exit(1)
         #! Use return for a pythonic and clean exit (avoid sys when possible)
         return
-
-    else:
-        arg1 = argv[1]
-
-        # run as Flask webUI
-        if arg1 == "server" or arg1 == None:
-            app.run()
-            # Process(target=app.run()).start()
-
-        # run as terminal UI 
-        elif arg1 == "terminal":
-            URL = ""
-
-            while URL != "q":
-                try:
-                    URL = input('\nEnter q to Quit:\n\
-                                 ````````````````\n\
-                                 Enter your URL: ')
-
-                    algorithmEngine = am.algorithmManager(URL) #algorithm object
-
-                    output  = "\nFishy?:" + str(algorithmEngine.run()) #fishy or not fishy boolean
-                    output += "\nEvaluation points:" + str(algorithmEngine.points)
-                    outputDict = algorithmEngine.createOutputString()
-                    
-                    for i in outputDict:
-                        print(i, outputDict[i])
-
-                except Exception as e:
-                    print(f"Error: {e}")
-
-        else:
-            print("Please don't, just use these two options:")
-            print(f"Usage1:  python {argv[0]} server")
-            print(f"Usage2:  python {argv[0]} terminal\n")
 
 if __name__ == '__main__':
     main()
